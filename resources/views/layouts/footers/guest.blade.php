a79bab9d1c24bae49eb87065dfe3e833a58446d7
Ricardo Monroy <rmonroy.rodriguez@gmail.com>
2020-06-03 18:41:28 -0500

    Estado inicial de Laravel 7 con Material Admin limpio

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-left">
+        
+        </nav>
+        <div class="copyright float-center">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, hecho con <i class="material-icons">favorite</i> por
+        <a href="#" target="_blank">Ricardo Monroy</a>
+        </div>
+    </div>
+</footer>
